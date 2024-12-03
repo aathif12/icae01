a57e321d72246deed457492dd0198460be024579
aathif12 <aathifahamad4@gmail.com>
2024-12-03 22:37:50 +0530

    Update Products naand product

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,36 +1,50 @@
-import "../assets/CSS/layout.css";
 import { useState } from "react";
-export default function Product(props) {
-  const [cart, setCart] = useState("");
+import "../assets/CSS/layout.css";
+
+export default function Product({ key, product, setCart, setTotal }) {
+  const [quantity, setQuantity] = useState(0);
+
+  const addToCart = () => {
+    if (quantity > 0) {
+      setCart((prev) => [
+        ...prev,
+        {
+          product: product.name,
+          quantity,
+          price: product.price,
+        },
+      ]);
+
+      setTotal((prevTotal) => prevTotal + product.price * quantity);
+    }
+  };
+
   return (
-    <>
-      {props.flowersp.map((flower) => {
-        return (
-          <div className="grid-item">
-            <div class="card">
-              <img src={require(`../assets/image/${flower.img}`)} />
-              <div class="card-body">
-                <h5 class="card-title">
-                  {flower.name} Price:{flower.price}
-                </h5>
-                <div class="quantity-container">
-                  <label for="quantity">Quantity:</label>
-                  <input type="number" id="quantity" name="quantity" />
-                </div>
-                <button
-                  class="card-button"
-                  id={flower.id}
-                  onClick={() => {
-                    setCart(flower);
-                  }}
-                >
-                  Add to Cart
-                </button>
-              </div>
-            </div>
+    <div className="grid-item" key={key}>
+      <div class="card">
+        <img
+          src={require(`../assets/image/${product.img}`)}
+          alt={product.name}
+        />
+        <div class="card-body">
+          <h5 class="card-title">{product.name} Price:</h5>
+          <div class="quantity-container">
+            <label for="quantity">Quantity:</label>
+            <input
+              type="number"
+              id="quantity"
+              name="quantity"
+              value={quantity}
+              onChange={(e) => {
+                setQuantity(e.target.value);
+              }}
+            />
           </div>
-        );
-      })}
-    </>
+          <button class="card-button" onClick={addToCart}>
+            Add to Cart
+          </button>
+        </div>
+      </div>
+    </div>
   );
 }
--- src/components/Products.js
+++ src/components/Products.js
@@ -1,8 +1,13 @@
+import { useState } from "react";
 import "../assets/CSS/layout.css";
 import Cart from "./Cart";
 import { flowers } from "./FlowerDB";
 import Product from "./Product";
+
 export default function Products() {
+  const [cart, setCart] = useState([]);
+  const [total, setTotal] = useState(0);
+
   return (
     <>
       <div className="item1">
@@ -11,10 +16,21 @@ export default function Products() {
       <div className="item2">
         <h4 className="card-title">Buy flowers</h4>
         <div className="grid-container">
-          <Product flowersp={flowers}></Product>
+          {flowers.map((flower, index) => {
+            return (
+              <Product
+                key={index}
+                product={flower}
+                setCart={setCart}
+                setTotal={setTotal}
+              />
+            );
+          })}
         </div>
       </div>
-      <div className="item3">{<Cart></Cart>}</div>
+      <div className="item3">
+        <Cart cart={cart} total={total} />
+      </div>
     </>
   );
 }
