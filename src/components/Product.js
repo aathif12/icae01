3c23c70bb2a6c0b43bcb399bfd82c5557f22c4ed
aathif12 <aathifahamad4@gmail.com>
2024-12-03 19:42:17 +0530

    Correct the Row issue

--- src/components/Product.js
+++ src/components/Product.js
@@ -2,24 +2,26 @@ import "../assets/CSS/layout.css";
 
 export default function Product(props) {
   return (
-    <div className="grid-item">
+    <>
       {props.flowersp.map((flower) => {
         return (
-          <div class="card">
-            <img src={require(`../assets/image/${flower.img}`)} />
-            <div class="card-body">
-              <h5 class="card-title">
-                {flower.name} Price:{flower.price}
-              </h5>
-              <div class="quantity-container">
-                <label for="quantity">Quantity:</label>
-                <input type="number" id="quantity" name="quantity" />
+          <div className="grid-item">
+            <div class="card">
+              <img src={require(`../assets/image/${flower.img}`)} />
+              <div class="card-body">
+                <h5 class="card-title">
+                  {flower.name} Price:{flower.price}
+                </h5>
+                <div class="quantity-container">
+                  <label for="quantity">Quantity:</label>
+                  <input type="number" id="quantity" name="quantity" />
+                </div>
+                <button class="card-button">Add to Cart</button>
               </div>
-              <button class="card-button">Add to Cart</button>
             </div>
           </div>
         );
       })}
-    </div>
+    </>
   );
 }
