32a479bd391db9f09c2ed23ad0b04d8dd76dfb62
aathif12 <aathifahamad4@gmail.com>
2024-12-03 19:31:41 +0530

    nmae and picture Showing

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,20 +1,25 @@
-import '../assets/CSS/layout.css';
-export default function Product(){
-    
-    return(
-        <div className="grid-item">
+import "../assets/CSS/layout.css";
 
-            <div class="card">
-                <img  />
-                <div class="card-body">
-                    <h5 class="card-title">Price:</h5>
-                    <div class="quantity-container">
-                        <label for="quantity">Quantity:</label>
-                        <input type="number" id="quantity" name="quantity" />
-                    </div>
-                    <button class="card-button">Add to Cart</button>
-                </div>
+export default function Product(props) {
+  return (
+    <div className="grid-item">
+      {props.flowersp.map((flower) => {
+        return (
+          <div class="card">
+            <img src={require(`../assets/image/${flower.img}`)} />
+            <div class="card-body">
+              <h5 class="card-title">
+                {flower.name} Price:{flower.price}
+              </h5>
+              <div class="quantity-container">
+                <label for="quantity">Quantity:</label>
+                <input type="number" id="quantity" name="quantity" />
+              </div>
+              <button class="card-button">Add to Cart</button>
             </div>
-        </div>
-    );
+          </div>
+        );
+      })}
+    </div>
+  );
 }
--- src/components/Products.js
+++ src/components/Products.js
@@ -1,29 +1,23 @@
-
-import '../assets/CSS/layout.css';
-
-
-export default function Products(){
-   
-    return(
-        <>
-            <div className="item1">
-                <h1>Flower Shop</h1>
-            </div>
-            <div className="item2">
-                <h4 className="card-title">Buy flowers</h4>
-                <div className="grid-container">
-                    {
-                        //product
-                    }
-                </div>
-
-            </div>
-            <div className="item3">
-                {
-                //cart
-                }
-            </div>
-        </>
-    );
-
+import "../assets/CSS/layout.css";
+import { flowers } from "./FlowerDB";
+import Product from "./Product";
+export default function Products() {
+  return (
+    <>
+      <div className="item1">
+        <h1>Flower Shop</h1>
+      </div>
+      <div className="item2">
+        <h4 className="card-title">Buy flowers</h4>
+        <div className="grid-container">
+          <Product flowersp={flowers}></Product>
+        </div>
+      </div>
+      <div className="item3">
+        {
+          //cart
+        }
+      </div>
+    </>
+  );
 }
